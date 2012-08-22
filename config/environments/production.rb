b4323269f4eb7352db7cb39bf2588cf324f79576
Adam Gamble <adamgamble@gmail.com>
2012-08-22 13:56:07 -0500

    [AG] more things

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 G2Advertising::Application.configure do
-  config.assets.compile = true
+  config.assets.compile = false
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
